6279851152625e37b374c649fcdebf2272541485
Мурат Кертиев <kertiev1107@gmail.com>
2024-06-01 17:44:28 +0300

    Применение купонов к заказам

--- my_shop/payment/webhooks.py
+++ my_shop/payment/webhooks.py
@@ -18,10 +18,8 @@ def stripe_webhook(request):
                     sig_header,
                     settings.STRIPE_WEBHOOK_SECRET)
     except ValueError as e:
-        # Invalid payload
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
         return HttpResponse(status=400)
 
     if event.type == 'checkout.session.completed':
